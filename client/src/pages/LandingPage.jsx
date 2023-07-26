11dbad5173ccfdaafb8a01bf21c573c900346884
tid213 <tid2136141@gmail.com>
2023-07-25 17:00:21 -0700

    added button for login and signup

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import greenMushroom from '../images/green-mushroom.png';
+import login from '../images/user.svg';
+import signup from '../images/user-plus.svg';
 import { useCookies } from "react-cookie";
 import axios from "axios";
 
@@ -17,10 +20,35 @@ const LandingPage = () => {
     }
 
     return(
-        <div className="home_page">
-            <h1>Welcome</h1>
-            <button onClick={signupButton}>Signup</button>
-            <button onClick={loginButton}>Login</button>
+        <div className="landing-page">
+            <div className="landing-title">
+              <h4>Welcome to..</h4>
+            </div>
+            <div className="landing-img">
+              <img src={greenMushroom}></img>
+            </div>
+            <div className="landing-name">
+            <p>Green Capz</p>
+            </div> 
+            <div className="landing-buttons">
+            <div onClick={loginButton} className="button-div">
+                <div className="button-img">
+                <img src={login}></img>
+                </div>
+                <div className="button-text">
+                <p>Sign in</p>
+                </div>
+            </div>
+            <div onClick={signupButton} className="button-div">
+                <div className="button-img">
+                <img src={signup}></img>
+                </div>
+                <div className="button-text">
+                <p>Sign up</p>
+                </div>
+            </div>
+            </div>
+            
         </div>
     )
 
