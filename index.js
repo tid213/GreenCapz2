a1bb350fea685bde4f1f25e0dc3becada9d32f37
tid213 <tid2136141@gmail.com>
2023-09-12 15:55:17 -0700

    added route to index file

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ app.use(express.json());
 
 app.use("/", authRoute);
 app.use("/", dataRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
