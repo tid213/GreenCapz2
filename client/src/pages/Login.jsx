93f0b5a7f6012e04399e62ec6fd9acb8238255de
tid213 <tid2136141@gmail.com>
2023-09-13 16:58:55 -0700

    didnt update for some reason

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://green-capz-c923aa3235e1.herokuapp.com:4000/login",
+        "https://green-capz-c923aa3235e1.herokuapp.com:4000/login", 
         {
           ...inputValue,
         },
