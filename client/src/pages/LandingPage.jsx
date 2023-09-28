e4b65597241e2f80516763fcac91b012627070cc
tid213 <tid2136141@gmail.com>
2023-09-28 16:05:55 -0700

    changed fonts and spacing for sign up div

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -63,6 +63,7 @@ const LandingPage = () => {
             <div className="product-info">
                 <ul>
                     <li><b>Optimize Plant Health:</b> Precise nutrient monitoring ensures that plants receive the ideal mix of nutrients, promoting robust growth, higher yields, and healthier crops.</li>
+                    <li><b>Resource Efficiency:</b> It minimizes nutrient waste, saving money and reducing environmental impact by using only what's necessary for plant growth.</li>
                     <li><b>Early Issue Detection:</b> Regular monitoring helps detect nutrient imbalances or deficiencies before they harm plants, allowing for timely adjustments and preventing crop loss.</li>
                     <li><b>Data-Driven Decisions:</b> Data collected aids in fine-tuning nutrient solutions, resulting in consistent and predictable crop outcomes.</li>
                     <li><b>Increased Productivity:</b> By maintaining optimal nutrient levels, growers can achieve faster growth cycles and more abundant harvests, increasing overall productivity in hydroponic systems.</li>
@@ -77,10 +78,13 @@ const LandingPage = () => {
                 <div className="landing-image"><img src={mushroomGroup}></img></div>
             </div>
             <div className="product-info-title-big"><h4>and Level Up</h4></div>
-            <div className="join-text">
-                <p>Let us help you level up your growing game and explore the future of cultivation with Green Capz! Log in or sign up today!</p>
-            </div>
             <div className="full-bar"></div>
+            <div className="join-text">
+                <p>Join <b>Green Capz</b> to level up your hydroponic growing experience! 
+                   Elevate your hydroponic journey, maximize efficiency, and unlock 
+                   your garden's full potential with <b>Green Capz</b>. Join us today and grow smarter!</p>
+            </div>
+            
             <div className="landing-buttons">
               <div onClick={loginButton} className="button-div">
                 <div className="button-img">
